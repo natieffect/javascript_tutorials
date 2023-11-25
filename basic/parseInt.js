1802f4d6763a87df6cbeb9241b94ae09a365cfbc
natinael <natnaelmasresha2022@gmail.com>
2023-11-25 09:43:02 +0300

    using the parse string valies in to int valies

---
+++ basic/parseInt.js
@@ -0,0 +1,16 @@
+//  java provides function to return an integer fron string 
+    //  parseInt(); used to chage the string to integer
+    //  if string can not be converted in to number return ---> NAN
+    let stringInt = 3;
+    let intString = parseInt(stringInt);
+
+
+// Use the parseInt function with a radix
+
+    // parseInt() functin parse string and return an intefer it takes second argumen for Radix
+    // specifies the base of the number in the string the radix can be 2 and 36
+    // parseInt(string,radix)
+
+    const a  = parseInt("11",2);
+    //radix  variable says that 11 is in the binary system or BASE 2
+    // this converts the string 11 to an intger 
